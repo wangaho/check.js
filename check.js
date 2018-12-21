9ae48ddf9e33e73d5b8c1280eb16ed5a6fff7ec1
WangAho <wangaho@anditogether.net>
2018-12-21 11:57:54 +0800

    浏览器兼容性不好则停止载入页面

--- check.js
+++ check.js
@@ -1,4 +1,5 @@
-if(!document.querySelectorAll || !w.FormData || !w.FormData.prototype.append){
+if(!document.querySelectorAll || !window.FormData || !window.FormData.prototype.append){
 	alert("您正在使用的浏览器版本过低，将不能正常浏览和使用。请升级IE10版本以上，或更换浏览器。");
-	throw SyntaxError();
+	window.stop ? window.stop() : document.execCommand("Stop");
+	throw new Error('您正在使用的浏览器版本过低，将不能正常浏览和使用。请升级IE10版本以上，或更换浏览器。');
 }
